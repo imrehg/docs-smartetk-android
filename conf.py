1d7237a89047cc130bc98dcd5500a10fb33ea9fc
Gergely Imreh <imrehg@gmail.com>
2015-10-27 18:09:14 +0800

    some config changes

--- conf.py
+++ conf.py
@@ -50,8 +50,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'SMART ETK Android API'
-copyright = u'2015, VIA Technologies'
+project = u'Smart ETK Android API'
+copyright = u'2015, VIA Technologies, Inc.'
 author = u'VIA Technologies'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -138,7 +138,7 @@ html_theme = 'bizstyle'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -152,7 +152,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
